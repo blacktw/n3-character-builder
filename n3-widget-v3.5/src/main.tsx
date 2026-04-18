a8f6c738653f6e4c9a1c5ecf482e7c64e9f893f8
Tom Black <blacktw@gmail.com>
2026-04-18 12:17:06 -0400

    Try to fix wrangler errors. Remove duplicate App.tsx; point to singular source of logic.

--- n3-widget-v3.5/src/main.tsx
+++ n3-widget-v3.5/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from '../../n3-widget-v3.5.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
